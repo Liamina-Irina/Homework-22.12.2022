762149b6c2726cca52b2141ce10ea50a23227554
Irina <liamina2207@gmail.com>
2022-12-22 23:51:20 +0300

    Solved Task 23

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// кубов чисел от 1 до N.
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0)
+{
+    TableCube(number);
+}
+else Console.WriteLine("Вы ввели ненатуральное число");
+
+void TableCube(int num)
+{
+   int count = 1;
+   while (count <= num)
+   {
+        Console.WriteLine($"{count, 6} {Math.Pow(count, 3), 6}");
+        count++;
+   }
+}
